7475774cf135d51ac23119ec37af175e1774f72e
Enkeym <julian1996@yandex.ru>
2023-05-25 12:36:43 +0300

    ready backend

---
+++ routes/employees.js
@@ -0,0 +1,13 @@
+const express  = require("express");
+const { auth } = require("../middleware/auth");
+const { all, add, remove, edit, employee } = require("../controllers/employees");
+
+const router = express.Router()
+
+router.get('/', auth, all)
+router.get('/:id', auth, employee)
+router.post('/add', auth, add)
+router.post('/remove/:id', auth, remove)
+router.put('/edit/:id', auth, edit)
+
+module.exports = router
